7ad1bf4af3a4ae3c5cebe7239a21c787f1d04174
Rakesh R <rakeshcap1185@gmail.com>
2024-06-07 14:39:07 +0530

    first commit

---
+++ gitfirst.js
@@ -0,0 +1,15 @@
+var fruit =["Apple","Orange","Grape","Banana"]
+/*console.log(fruit[0])
+console.log(fruit[1])
+console.log(fruit[2])
+console.log(fruit[3])*/
+
+// for(count=0;count<=5;count=count+1)
+// {
+//     console.log(count)
+// }
+console.log(fruit.length)
+for(count=0;count<fruit.length;count=count+1)
+    {
+        console.log(fruit[count])
+    }
